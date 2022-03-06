f3bf15f33d3ffa4de148794a9851a008825b8140
zetgi1 <36907577+zetgi1@users.noreply.github.com>
2022-03-05 19:58:26 -0500

    edited

--- cases.sql
+++ cases.sql
@@ -1,56 +1,35 @@
 #mysql --local-infile=1 -u root -p
-SHOW DATABASES;
-CREATE DATABASE covid_app;
 USE covid_app;
 
+drop table conf_orig;
+SET SQL_SAFE_UPDATES = 0;
 CREATE TABLE conf_orig (
+    c_date VARCHAR(50),
     iso2 VARCHAR(5),
     iso3 VARCHAR(5),
     code3 INT,
     FIPS INT,
     Province_State VARCHAR(100),
     Country_Region VARCHAR (5),
-    Population INT,
-    c_date DATE,
     Cases INT
     );
 
-SHOW TABLES;
-
-
-LOAD DATA LOCAL INFILE 'C:\Users\zetgi\Desktop\Database\new_confirmed.csv' 
+LOAD DATA local INFILE 'C:/Users/zetgi/Desktop/Database/new_confirmed.csv' 
 INTO TABLE conf_orig 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (@iso2, @iso3, @code3, @FIPS, @Province_State, @Country_Region, @Population, @c_Date, @Cases)
-SET c_date  = STR_TO_DATE(@c_date, '%c/%d/%Y'),
-	iso3 = if(@iso3="", NULL, @iso3),
-	code3 = if(@code3="", NULL, @code3),
-	FIPS = if(@FIPS="", NULL, @FIPS),
-	Province_State = if(@Province_State="", NULL, @Province_State),
-	Country_Region = if(@Country_Region="", NULL, @Country_Region),
-	Population = if(@Population="", NULL, @Population),
-	Cases = if(@Cases="", NULL, @Cases);
-    
-    
-CREATE TABLE state_info (
-	state_pop_id INT NOT NULL AUTO_INCREMENT,
-    FIPS INT,
-    population INT,
-    PRIMARY KEY (state_pop_id)
-)
-	SELECT FIPS, population
-    FROM conf_orig
-    Where level = 'State'
-    GROUP BY FIPS, population;
-    
+IGNORE 1 ROWS
+;
+UPDATE conf_orig SET c_date = STR_TO_DATE(c_date, '%m/%d/%Y');
+select *from conf_orig;
 
+drop table cases;
 CREATE TABLE cases (
 	FIPS INT,
-    c_date DATE,
+    c_date VARCHAR(50),
 	Cases INT
 )
 	SELECT FIPS, c_date, Cases
-	FROM conf_orig
-	WHERE level = 'State';
-
+	FROM conf_orig;
+show tables;
+select *from cases;
--- deaths.sql
+++ deaths.sql
@@ -1,35 +1,41 @@
 #mysql --local-infile=1 -u root -p
-SHOW DATABASES;
-CREATE DATABASE covid_app;
-USE covid_app;
+SET SQL_SAFE_UPDATES = 0;
 
+USE covid_app;
+drop table death_orig;
 CREATE TABLE death_orig (
+    c_date VARCHAR(50),
     iso2 VARCHAR(5),
     iso3 VARCHAR(5),
     code3 INT,
     FIPS INT,
     Province_State VARCHAR(100),
     Country_Region VARCHAR (5),
-    c_date DATE,
-    Deaths INT
+    Population INT,
+    Death INT
     );
 
-SHOW TABLES;
 
 
-LOAD DATA LOCAL INFILE 'C:\Users\zetgi\Desktop\Database\new_death.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/zetgi/Desktop/Database/new_death.csv' 
 INTO TABLE death_orig 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (@iso2, @iso3, @code3, @FIPS, @Province_State, @Country_Region, @c_Date, @Deaths)
-SET c_date  = STR_TO_DATE(@c_date, '%c/%d/%Y'),
-	iso3 = if(@iso3="", NULL, @iso3),
-	code3 = if(@code3="", NULL, @code3),
-	FIPS = if(@FIPS="", NULL, @FIPS),
-	Province_State = if(@Province_State="", NULL, @Province_State),
-	Country_Region = if(@Country_Region="", NULL, @Country_Region),
-	Deaths = if(@Deaths="", NULL, @Deaths);
-    
+IGNORE 1 ROWS
+;
+UPDATE death_orig SET c_date = STR_TO_DATE(c_date, '%m/%d/%Y');
+select * from death_orig;
+
+
+drop table state_info;
+create table state_info(
+	FIPS INT,
+    Population INT
+    )
+		select FIPS, Population
+        from death_orig
+        group by fips, population;
+select * from state_info;
 
 CREATE TABLE Deaths (
 	FIPS INT,
@@ -37,6 +43,5 @@ CREATE TABLE Deaths (
 	Deaths INT
 )
 	SELECT FIPS, c_date, Deaths
-	FROM death_orig
-	WHERE level = 'State';
+	FROM death_orig;
 
